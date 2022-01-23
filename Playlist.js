ac10c0fa9e63b049c13f85e30ae2ec96448d96d3
Demba Diack <dembadiack@outlook.com>
2022-01-23 11:42:14 +0000

    magic

--- Playlist.js
+++ Playlist.js
@@ -2,13 +2,13 @@
 const SpotifyWebApi = require('spotify-web-api-node');
 const { searchTracks } = require('./app');
 const spotifyApi = new SpotifyWebApi({
-    clientId: 'be1775db22fa46379449544b2c4aff9c',
-    clientSecret: '64315f8c2be44102ba162baf0eea6d35'
+    clientId: '',
+    clientSecret: ''
 });
 let tracks = [];
 
 
-spotifyApi.setAccessToken("BQB1wPX8KV_vRv2ql1EW2JSi5RWACTV4mFguVv8N3d18XPNMCDKGetYPa9-6ebqupUAW7sf7NzHfuF6ZBPLMYrOGhHw2h7uYM7bggQ9um470bNoFyw4OxM31UG2tYa7ts4W4WfGExdYP7MYeU8ngwNaqYnEjrQssWOK8HRlKB60q3wxbtc6OsTg&refresh_token=AQAbI6ux--bqmsR42DRjSKQ4C0qysMh6sa0Pam8F9pg4YP_Dy_2kyP-NsFBA6sE9SObqe0Rhff4R1obFpGYrqCjacxcd37YjooLAn-JHM91HxQQJO91QK_Zxj_KHex4O60c");
+spotifyApi.setAccessToken("");
 
 var lines = require('fs').readFileSync("database.txt", 'utf-8')
     .split('\n')
